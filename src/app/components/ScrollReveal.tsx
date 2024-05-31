ad1a33681e3c793fa5c6015821d59c9c066f410f
Rei2718 <129850397+Rei2718@users.noreply.github.com>
2024-05-31 16:46:30 +0900

    ScrollReveal.tsx を更新

--- src/app/components/ScrollReveal.tsx
+++ src/app/components/ScrollReveal.tsx
@@ -1,7 +1,6 @@
-"use client";
+"use client"
 
 import { FC, useRef, useEffect } from "react";
-import ScrollReveal from "scrollreveal";
 
 interface ScrollRevealContainerProps {
   move?: string;
@@ -15,33 +14,49 @@ const ScrollRevealContainer: FC<ScrollRevealContainerProps> = ({
   const sectionRef = useRef<HTMLElement>(null);
 
   useEffect(() => {
-    const section = sectionRef.current;
-    if (!section) return;
+    const revealSection = async () => {
+      // ブラウザ環境でのみ scrollReveal を実行する
+      if (typeof window !== "undefined" && sectionRef.current) {
+        const ScrollReveal = await import("scrollreveal");
+        const sr = ScrollReveal.default();
 
-    // Initial styles
-    section.style.opacity = "0";
-    section.style.filter = "blur(4px)";
+        const section = sectionRef.current;
 
-    const revealOptions = {
-      reset: true,
-      delay: 300,
-      opacity: 0, // Initial opacity
-      origin: move || "bottom",
-      distance: "20px",
-      duration: 600,
-      easing: "ease-in-out",
-      beforeReveal: (el: HTMLElement) => {
-        el.style.transition = "opacity 1s ease, filter 1s ease";
-        el.style.opacity = "1";
-        el.style.filter = "blur(0px)";
-      },
-      beforeReset: (el: HTMLElement) => {
-        el.style.opacity = "0";
-        el.style.filter = "blur(4px)";
+        if (section) {
+          // 初期状態で文字を非表示にする
+          section.style.opacity = "0";
+          // 初期状態で文字にブラーをかける
+          section.style.filter = "blur(4px)";
+        }
+
+        sr.reveal(sectionRef.current, {
+          reset: true,
+          delay: 300,
+          opacity: 1, // アニメーション後の最終的な透明度
+          origin: move || "bottom",
+          distance: "5px",
+          beforeReveal: el => {
+            // アニメーション前の処理
+            if (el instanceof HTMLElement) {
+              // ブラーを解除して文字を浮かび上がらせるアニメーション
+              el.style.transition = "opacity 1s ease, filter 1s ease";
+              el.style.opacity = "1";
+              el.style.filter = "blur(0px)";
+            }
+          },
+          beforeReset: el => {
+            // アニメーション後の処理
+            if (el instanceof HTMLElement) {
+              // ブラーを再びかけて文字を非表示にする
+              el.style.opacity = "0";
+              el.style.filter = "blur(4px)";
+            }
+          }
+        });
       }
     };
 
-    ScrollReveal().reveal(section, revealOptions);
+    revealSection();
   }, [move]);
 
   return <section ref={sectionRef}>{children}</section>;
