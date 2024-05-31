4db446dd9a9bbeea544f86294b2b629db3de3684
Rei2718 <129850397+Rei2718@users.noreply.github.com>
2024-06-01 07:36:56 +0900

    Suspence by-black

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -94,7 +94,7 @@ function HomeContent() {
 
 export default function Home() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<div className="bg-black">Loading...</div>}>
       <HomeContent />
     </Suspense>
   );
