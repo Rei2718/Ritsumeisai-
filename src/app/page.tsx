1b00d8aac8a378d80dacbb2c4bd505a63b3f8da7
Rei2718 <129850397+Rei2718@users.noreply.github.com>
2024-05-31 16:01:54 +0900

    h-screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <>
         <Header />
-        <img src="BG02.jpg" alt="BgImg" className="object-cover h-school w-screen fixed top-0 left-0 z-0"/>
+        <img src="BG02.jpg" alt="BgImg" className="object-cover h-screen w-screen fixed top-0 left-0 z-0"/>
         <div className="relative z-10 bg-gradient-css">
           {/* Section 1 */}
           <section className="flex items-center justify-center h-screen px-10">
