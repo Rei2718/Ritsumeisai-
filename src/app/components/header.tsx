1de76361983fc2e75782237020a8b2189718c075
Rei2718 <129850397+Rei2718@users.noreply.github.com>
2024-05-31 15:55:29 +0900

    Update header

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -31,10 +31,9 @@ export default function Header() {
   
   return (
     <>
-      {/* Header */}
-      <header className="z-50 fixed top-0 w-full">
-        <div className="flex justify-between items-center h-full w-full bg-clip-padding">
-　　　　　　　<a onClick={() => handleLinkClick('/')}>
+        {/* Header */}
+        <header className="z-50 fixed top-0 w-full">
+          <div className="flex justify-between items-center h-full w-full bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-0">
             <div className="py-3 px-3 flex justify-start items-center">
               <img
                 src="logo.png"
@@ -42,24 +41,21 @@ export default function Header() {
                 className="w-10 h-10 bg-transparent cursor-pointer"
               />
             </div>
-　　　　　　　</a>
-          <div className="flex-grow flex justify-center items-center">
             <div className='text-lg text-white'>Ritsumeikan Keisho</div>
+            <div className="py-3 px-3 flex justify-end items-center">
+              <button
+                onClick={toggleMenu}
+                className="text-white focus:outline-none z-50 relative w-10 h-10 px-1"
+              >
+                <div className={`w-8 h-0.5 bg-white mb-1.5 transition-all duration-700 ${isMenuOpen ? 'transform rotate-45 translate-y-2' : ''}`}></div>
+                <div className={`w-8 h-0.5 bg-white mb-1.5 transition-all duration-700 ${isMenuOpen ? 'opacity-0' : ''}`}></div>
+                <div className={`w-8 h-0.5 bg-white transition-all duration-700 ${isMenuOpen ? 'transform -rotate-45 -translate-y-2' : ''}`}></div>
+              </button>
+            </div>
           </div>
-          <div className="py-3 px-3 flex justify-end items-center">
-            <button
-              onClick={toggleMenu}
-              className="text-white focus:outline-none z-50 relative w-10 h-10 px-1"
-            >
-              <div className={`w-8 h-0.5 bg-white mb-1.5 transition-all duration-700 ${isMenuOpen ? 'transform rotate-45 translate-y-2' : ''}`}></div>
-              <div className={`w-8 h-0.5 bg-white mb-1.5 transition-all duration-700 ${isMenuOpen ? 'opacity-0' : ''}`}></div>
-              <div className={`w-8 h-0.5 bg-white transition-all duration-700 ${isMenuOpen ? 'transform -rotate-45 -translate-y-2' : ''}`}></div>
-            </button>
-          </div>
-        </div>
-        <div className="w-full border-t border-white/5"></div>
-      </header>
-      {/* Header_END */}
+          <div className="w-full border-t border-white/5"></div>
+        </header>
+        {/* Header_END */}
 
       {/* Fullscreen Menu */}
       <div className={`fixed inset-0 bg-black bg-opacity-90 flex items-center justify-center z-40 transition-opacity duration-700 ${isMenuOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'}`}>
