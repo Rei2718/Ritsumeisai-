5a2d2fc5139441dd85516e21cefa266ab28c53d2
Rei2718 <once2718@gmail.com>
2024-06-01 00:04:41 +0900

    Update the icon settings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,13 +17,13 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <head>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-        <link rel="manifest" href="/site.webmanifest"/>
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
-        <meta name="msapplication-TileColor" content="#da532c"/>
-        <meta name="theme-color" content="#ffffff"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png"/>
+        <link rel="manifest" href="/icons/site.webmanifest"/>
+        <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#81D8D0"/>
+        <meta name="msapplication-TileColor" content="##81D8D0"/>
+        <meta name="theme-color" content="#81D8D0"/>
       </head>
       <body className={inter.className}>
         <div className="font-mono">{children}</div>
