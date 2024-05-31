ac8dc8c95930b0d83128bb8f8f6fbcd121ef03a7
Rei2718 <129850397+Rei2718@users.noreply.github.com>
2024-06-01 07:42:17 +0900

    page.tsx を更新

--- src/app/section01/page.tsx
+++ src/app/section01/page.tsx
@@ -8,7 +8,6 @@ function Section01Content() {
   return (
     <>
       <Header />
-      <img src="BG02.jpg" alt="BgImg" className="object-cover w-screen h-lvh fixed top-0 left-0 z-0"/>
       <div className="relative z-10 bg-gradient-css">
         {/* Section 1 */}
         <section className="flex items-center justify-center h-lvh">
