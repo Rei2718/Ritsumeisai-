b032cdf6f183c4d2daa941f438cf53f11eb4f032
Rei2718 <once2718@gmail.com>
2024-06-01 21:20:39 +0900

    Added the menue compunents

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -47,7 +47,7 @@ export default function Header() {
           <button onClick={() => handleLinkClick('/')}>
             <div className="py-3 px-3 flex justify-start items-center">
               <img
-                src="logo.png"
+                src="../logo.png"
                 alt="logo"
                 className="w-10 h-10 bg-transparent cursor-pointer"
               />
@@ -71,30 +71,18 @@ export default function Header() {
 
       {/* Fullscreen Menu */}
       <div className={`fixed inset-0 bg-black bg-opacity-90 flex items-center justify-center z-40 transition-opacity duration-700 ${isMenuOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'}`}>
-        <ul className="text-white text-2xl text-center">
+        <ul className="text-white text-xl text-center">
           {/* HOME */}
-          <li className='py-1'><button onClick={() => handleLinkClick('/')}>Home</button></li>
+          <li className='py-1.5'><button onClick={() => handleLinkClick('/')}>トップ</button></li>
 
-          {/* Section01 */}
-          <li className='py-1'>
-            <div className="flex flex-col items-center transform -translate-x-2.5">
-              <button onClick={() => toggleSubMenu('section01')} className="flex items-center">
-                <span className={`mr-2 text-sm transition-transform duration-500 ${activeMenu === 'section01' ? 'rotate-180' : 'rotate-0'}`}>              
-                  <svg className="w-[10px] h-[10px] fill-gray-400" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"></path>
-                  </svg>
-                </span>
-                <span className="flex-grow text-center">Section1</span>
-              </button>
-              <ul className={`overflow-hidden transition-all duration-700 text-center ${activeMenu === 'section01' ? 'max-h-40 opacity-100 blur-none' : 'max-h-0 opacity-0 blur-sm'}`}>
-                <li className='py-1'><button onClick={() => handleLinkClick('/section01/sub1')}>Subsection 1-1</button></li>
-                <li className='py-1'><button onClick={() => handleLinkClick('/section01/sub2')}>Subsection 1-2</button></li>
-              </ul>
-            </div>
-          </li>
+          <li className='py-1.5'><button onClick={() => handleLinkClick('/kitchen')}>キッチンカー</button></li>
+
+          <li className='py-1.5'><button onClick={() => handleLinkClick('/arena')}>アリーナ有志</button></li>
+
+          <li className='py-1.5'><button onClick={() => handleLinkClick('/cotan')}>Co-Tan BP</button></li>
 
           {/* Section02 */}
-          <li className="py-1">
+          <li className="py-1.5">
             <div className="flex flex-col items-center transform -translate-x-2.5">
               <button onClick={() => toggleSubMenu('section02')} className="flex items-center">
                 <span className={`mr-2 text-sm transition-transform duration-500 ${activeMenu === 'section02' ? 'rotate-180' : 'rotate-0'}`}>
@@ -102,17 +90,18 @@ export default function Header() {
                     <path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"></path>
                   </svg>
                 </span>
-                <span className="flex-grow text-center">Section2</span>
+                <span className="flex-grow text-center">中学クラス企画</span>
               </button>
               <ul className={`overflow-hidden transition-all duration-700 text-center ${activeMenu === 'section02' ? 'max-h-40 opacity-100 blur-none' : 'max-h-0 opacity-0 blur-sm'}`}>
-                <li className='py-1'><button onClick={() => handleLinkClick('/section02/sub1')}>Subsection 2-1</button></li>
-                <li className='py-1'><button onClick={() => handleLinkClick('/section02/sub2')}>Subsection 2-2</button></li>
+                <li className='py-1.5'><button onClick={() => handleLinkClick('/jhs/j1')}>中1企画</button></li>
+                <li className='py-1.5'><button onClick={() => handleLinkClick('/jhs/j2')}>中2企画</button></li>
+                <li className='py-1.5'><button onClick={() => handleLinkClick('/jhs/j3')}>中3企画</button></li>
               </ul>
             </div>
           </li>
 
           {/* Section03 */}
-          <li className="py-1">
+          <li className="py-1.5">
             <div className="flex flex-col items-center transform -translate-x-2.5">
               <button onClick={() => toggleSubMenu('section03')} className="flex items-center">
                 <span className={`mr-2 text-sm transition-transform duration-500 ${activeMenu === 'section03' ? 'rotate-180' : 'rotate-0'}`}>
@@ -120,11 +109,12 @@ export default function Header() {
                     <path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"></path>
                   </svg>
                 </span>
-                <span className="flex-grow text-center">Section3</span>
+                <span className="flex-grow text-center">高校クラス企画</span>
               </button>
               <ul className={`overflow-hidden transition-all duration-700 text-center ${activeMenu === 'section03' ? 'max-h-40 opacity-100 blur-none' : 'max-h-0 opacity-0 blur-sm'}`}>
-                <li className='py-1'><button onClick={() => handleLinkClick('/section03/sub1')}>Subsection 3-1</button></li>
-                <li className='py-1'><button onClick={() => handleLinkClick('/section03/sub2')}>Subsection 3-2</button></li>
+                <li className='py-1.5'><button onClick={() => handleLinkClick('/shs/s1')}>高1企画</button></li>
+                <li className='py-1.5'><button onClick={() => handleLinkClick('/shs/s2')}>高2企画</button></li>
+                <li className='py-1.5'><button onClick={() => handleLinkClick('/shs/s3')}>高3企画</button></li>
               </ul>
             </div>
           </li>
@@ -132,7 +122,7 @@ export default function Header() {
       </div>
 
       {/* BgImg */}
-      <img src="BG02.jpg" alt="BgImg" className="object-cover w-screen h-lvh fixed top-0 left-0 z-0"/>
+      <img src="../BG02.jpg" alt="BgImg" className="object-cover w-screen h-lvh fixed top-0 left-0 z-0"/>
     </>
   );
 }
